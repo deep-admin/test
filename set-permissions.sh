3bb5862bead4b994283796c6795981e0a68f5aae
deep-admin <deep@captalnumbers.com>
2021-07-12 19:44:24 +0530

    add2

--- set-permissions.sh
+++ set-permissions.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo chmod -R 775 bootstrap/cache/ storage/
-sudo chown -R ec2-user:apache  bootstrap/cache/ storage
+sudo chmod -R 775 /var/www/html/bootstrap/cache/ 
+sudo chmod -R 775 /var/www/html/storage/
+sudo chown -R ec2-user:apache  /var/www/html/bootstrap/cache/ 
+sudo chown -R ec2-user:apache  /var/www/html/storage/
